71a8b53a3ad57e876c9c56f52170a21511c72e88
Shubham Kumar <112159406+shubham-kumar-2004@users.noreply.github.com>
2024-10-09 21:57:59 +0530

    TransfiiNITTe is Live

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,99 +1,152 @@
-import React, { lazy, memo, Suspense } from "react";
-import { arrowbl, arrowwh, numbersvg, tfbadge } from "../assets";
-import { toast } from "sonner";
-import { Button } from "./ui/button";
+import { arrowwh, numbersvg, tfbadge } from "../assets";
+import Flip from "./Flip";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
-const Flip = lazy(() => import("./Flip"));
-
-const Header = memo(() => {
-  const handleClick = () => {
-    toast("Coming Soon", {
-      description: "Tuesday, 8 October 2024, 9:00 PM", // Custom style for description
-      style: {
-        backgroundColor: "#EDEDED",
-        color: "#000",
-        padding: "16px",
-      },
-      className: "font-spacemono",
-    });
-  };
-
-  const RegisterButton = ({ variant, className }) => (
-    <Button variant={variant} onClick={handleClick}>
-      <div
-        className={`flex flex-row gap-1 justify-center items-center bg-offwhite px-2 py-0 rounded-full hover:underline decoration-black ${className}`}
-      >
-        <div className="text-black text-center font-spacemono text-[0.875rem] not-italic font-bold leading-5 tracking-[0.35px] uppercase bg-offwhite">
-          REGISTER
-        </div>
-        <div>
-          <img src={arrowbl} className="max-w-none w-fit bg-offwhite" />
-        </div>
-      </div>
-    </Button>
-  );
-
-  const InstagramButton = () => (
-    <a
-      href="https://www.instagram.com/tc_nitt/"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="hover:underline decoration-white"
-    >
-      <button className="flex flex-row gap-1 justify-center items-center">
-        <div className="text-offwhite text-center font-spacemono text-[0.875rem] not-italic font-bold leading-5 tracking-[0.35px] uppercase">
-          INSTAGRAM
-        </div>
-        <div>
-          <img src={arrowwh} className="max-w-none w-fit" />
-        </div>
-      </button>
-    </a>
-  );
-
-  const HeaderMobile = memo(() => (
-    <div className="block sm:hidden relative">
-      <div className="flex items-center justify-between bg-black shrink-0 w-full border-b-[#EDEDED26] border-b border-solid py-4 px-8 sticky top-0 shadow-md z-50 content-stretch">
-        <Suspense fallback={<div>Loading...</div>}>
-          <img src={tfbadge} />
-        </Suspense>
-        <RegisterButton variant="outline" className="px-[0.5625rem] py-1" />
-      </div>
-    </div>
-  ));
-
-  const HeaderDesktop = memo(() => (
-    <div className="hidden sm:flex justify-between items-center h-max py-5 bg-black shrink-0 self-stretch border-b-[#EDEDED26] border-b border-solid pl-12 sticky top-0 shadow-md z-50 content-stretch">
-      <div className="w-[15vw]">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Flip />
-        </Suspense>
-      </div>
-      <div className="flex flex-row flex-grow gap-1 self-stretch items-center justify-center">
-        <div className="text-white text-center text-xl font-spacegrotesk items-center font-medium leading-5 tracking-[-0.1px] uppercase">
-          Transfinitte
-        </div>
-        <div>
-          <a>
-            <img src={numbersvg} alt="24" />
-          </a>
-        </div>
-      </div>
-      <div className="w-fit h-fit flex gap-[36px] mr-[3rem] justify-center items-center">
-        <InstagramButton />
-        <RegisterButton variant="outline" className="px-5 py-2" />
-      </div>
-    </div>
-  ));
+const Header = () => {
 
   return (
-    <div className="sticky top-0 z-50 select-none">
-      <Suspense fallback={<div>Loading...</div>}>
-        <HeaderMobile />
-        <HeaderDesktop />
-      </Suspense>
+    <div className="sticky top-0 z-50">
+      {/* Mobile Version */}
+      <div className="block sm:hidden relative">
+        <div className="flex items-center justify-between bg-black shrink-0 w-full border-b-[#EDEDED26] border-b border-solid py-4 px-8 sticky top-0 shadow-md z-50 content-stretch">
+          <img src={tfbadge} />
+          <div className="inline-flex px-[0.5rem] py-[0.63rem] justify-center items-center gap-[0.625rem] rounded-full border-[0.5px] border-solid border-footerbutton footer-links hover:underline decoration-white leading-none my-0">
+  <AlertDialog>
+    <AlertDialogTrigger asChild>
+      <div className="flex flex-row gap-1 justify-center items-center">
+        <div className="text-offwhite text-center font-spacemono text-[0.875rem] not-italic font-bold tracking-[0.35px] uppercase">
+          REGISTER
+        </div>
+        <img src={arrowwh} className="w-auto h-2" />
+      </div>
+    </AlertDialogTrigger>
+
+    <AlertDialogContent className="w-auto max-w-[90%] opacity-100 bg-offwhite rounded-3xl">
+      <AlertDialogHeader>
+        <AlertDialogDescription>
+          <div className="flex flex-col items-center gap-4 self-stretch">
+            <div className="self-stretch text-black font-spacegrotesk text-[2rem] not-italic font-medium leading-[-0.02rem]">
+              Are you sure?
+            </div>
+            <div className="self-stretch text-black font-spacegrotesk text-[1rem] not-italic font-light leading-[-0.015rem]">
+            You are being redirected to <span className="font-bold">register.transfinitte.com</span> to continue with the registration. Do you wish to proceed?
+<br/><br/>Note: The registration may not work if you are connected to the institute wifi (HP). Please make sure you are using an alternate, stable network connection.
+<br/></div>
+          </div>
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+
+      <AlertDialogFooter>
+        <AlertDialogCancel>
+          <div className="flex flex-row gap-1 justify-center items-center border-solid border-footerbutton border-black border-2 p-2 rounded-3xl px-5 footer-links py-[6px] ">
+            <div className="text-black text-center font-spacemono text-[0.875rem] not-italic font-bold leading-5 tracking-[0.35px] uppercase">
+              Cancel
+            </div>
+          </div>
+        </AlertDialogCancel>
+        <AlertDialogAction onClick={() => window.location.href = 'https://register.transfinitte.com'}>
+          <div className="flex flex-row gap-1 justify-center items-center border-solid border-footerbutton border-black border-2 p-2 rounded-3xl px-5 footer-links py-[6px] ">
+            <div className="text-black text-center font-spacemono text-[0.875rem] not-italic font-bold leading-5 tracking-[0.35px] uppercase">
+              Continue
+            </div>
+          </div>
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</div>
+        </div>
+      </div>
+
+      {/* Desktop version */}
+      <div className="hidden sm:flex justify-between items-center h-max py-5 bg-black shrink-0 self-stretch border-b-[#EDEDED26] border-b border-solid pl-12 sticky top-0 shadow-md z-50 content-stretch">
+        <div className="w-[15vw]">
+          <Flip />
+        </div>
+        <div className="flex flex-row flex-grow gap-1 self-stretch items-center justify-center">
+          <div className="text-white text-center text-xl font-spacegrotesk items-center font-medium leading-5 tracking-[-0.1px] uppercase">
+            Transfinitte
+          </div>
+          <div>
+            <a>
+              <img src={numbersvg} alt="24" />
+            </a>
+          </div>
+        </div>
+        <div className="w-fit h-fit flex gap-[36px] mr-[3rem] justify-center items-center">
+          <a
+            href="https://www.instagram.com/tc_nitt/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline decoration-white"
+          >
+            <button className="flex flex-row gap-1 justify-center items-center">
+              <div className="text-offwhite text-center font-spacemono text-[0.875rem] not-italic font-bold leading-5 tracking-[0.35px] uppercase">
+                INSTAGRAM
+              </div>
+              <div>
+                <img src={arrowwh} className="max-w-none w-fit" />
+              </div>
+            </button>
+          </a>
+          <div className="inline-flex px-[0.5rem] py-[0.63rem] justify-center items-center gap-[0.625rem] rounded-full border-[0.5px] border-solid border-footerbutton footer-links hover:underline decoration-white leading-none my-0">
+  <AlertDialog>
+    <AlertDialogTrigger asChild>
+      <div className="flex flex-row gap-1 justify-center items-center">
+        <div className="text-offwhite text-center font-spacemono text-[0.875rem] not-italic font-bold tracking-[0.35px] uppercase">
+          REGISTER
+        </div>
+        <img src={arrowwh} className="w-auto h-2" />
+      </div>
+    </AlertDialogTrigger>
+
+    <AlertDialogContent className="w-auto max-w-[90%] opacity-100 bg-offwhite rounded-3xl">
+      <AlertDialogHeader>
+        <AlertDialogDescription>
+          <div className="flex flex-col items-center gap-4 self-stretch">
+            <div className="self-stretch text-black font-spacegrotesk text-[2rem] not-italic font-medium leading-[-0.02rem]">
+              Are you sure?
+            </div>
+            <div className="self-stretch text-black font-spacegrotesk text-[1rem] not-italic font-light leading-[-0.015rem]">
+            You are being redirected to <span className="font-bold">register.transfinitte.com</span> to continue with the registration. Do you wish to proceed?
+<br/><br/>Note: The registration may not work if you are connected to the institute wifi (HP). Please make sure you are using an alternate, stable network connection.
+<br/></div>
+          </div>
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+
+      <AlertDialogFooter>
+        <AlertDialogCancel>
+          <div className="flex flex-row gap-1 justify-center items-center border-solid border-footerbutton border-black border-2 p-2 rounded-3xl px-5 footer-links py-[6px] ">
+            <div className="text-black text-center font-spacemono text-[0.875rem] not-italic font-bold leading-5 tracking-[0.35px] uppercase">
+              Cancel
+            </div>
+          </div>
+        </AlertDialogCancel>
+        <AlertDialogAction onClick={() => window.location.href = 'https://register.transfinitte.com'}>
+          <div className="flex flex-row gap-1 justify-center items-center border-solid border-footerbutton border-black border-2 p-2 rounded-3xl px-5 footer-links py-[6px] ">
+            <div className="text-black text-center font-spacemono text-[0.875rem] not-italic font-bold leading-5 tracking-[0.35px] uppercase">
+              Continue
+            </div>
+          </div>
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</div>
+        </div>
+      </div>
     </div>
   );
-});
+};
 
 export default Header;
